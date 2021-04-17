be436d377c70e9f9d1fc161acc301860d40f4c2b
MalkavianxX <57018054+MalkavianxX@users.noreply.github.com>
2021-04-16 23:25:27 -0500

    Update settings.py

--- WEB/settings.py
+++ WEB/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'w*-q%j_b=xhvw323we-lpaoh$c3q-1r%zurc_0s)!*nv$%!@c-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
